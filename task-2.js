d6eddb9acab532a3694b1c06c5fd1a4712528363
Che1sea57 <38821256+Che1sea57@users.noreply.github.com>
2019-03-10 00:19:38 +0300

    переделал второе задание

--- task-2.js
+++ task-2.js
@@ -1,26 +1,36 @@
 (function () {
     'use strict';
 
-    let numbers = [1, 1, 2, 3, 4, 3];
+    let numbers = [1, 2, 3, 4, 3, 1];
+
+    // function removeDuplicates(arr) {
+    //     let duplicates = {},
+    //     result = [];
+    //     for (let number of arr) {
+    //         if (duplicates.hasOwnProperty(number)) {
+    //             duplicates[number] = 'repeating';
+    //         } else {
+    //             duplicates[number] = 'single';
+    //         }
+    //     }
+    //     for (let numbers in duplicates ) {
+    //         if (duplicates[numbers] ==='repeating') {
+    //             delete duplicates[numbers];
+    //         }
+    //     }
+    //     let resultOfStr = Object.keys(duplicates);
+    //     for (let item of resultOfStr) {
+    //         result.push(parseFloat(item));
+    //     }
+    //     return result;
+    // }
 
     function removeDuplicates(arr) {
-        let duplicates = {},
-        result = [];
+        let result = [];
         for (let number of arr) {
-            if (duplicates.hasOwnProperty(number)) {
-                duplicates[number] = 'repeating';
-            } else {
-                duplicates[number] = 'single';
+            if (arr.indexOf(number) === arr.lastIndexOf(number)) {
+                result.push(number)
             }
-        }
-        for (let numbers in duplicates ) {
-            if (duplicates[numbers] ==='repeating') {
-                delete duplicates[numbers];
-            }
-        }
-        let resultOfStr = Object.keys(duplicates);
-        for (let item of resultOfStr) {
-            result.push(parseFloat(item));
         }
         return result;
     }
